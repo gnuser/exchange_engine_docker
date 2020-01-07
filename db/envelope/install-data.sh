c0d1ce15af2b21fbbdc1908304c45ac0aa8925f5
ubuntu <ubuntu@localhost.localdomain>
2020-01-08 00:22:30 +0800

    调整微交易sql配置

--- db/envelope/install-data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-MYSQL_ROOT_PASSWORD="root"
-
-#!/bin/bash
-mysql -uroot -p$MYSQL_ROOT_PASSWORD <<EOF
-source $WORK_PATH/$FILE_0;
-EOF
-#
--- db/sql/install-data.sh
+++ db/sql/install-data.sh
@@ -6,6 +6,14 @@ mysql -uroot -p$MYSQL_ROOT_PASSWORD <<EOF
 source $WORK_PATH/$FILE_0;
 source $WORK_PATH/$FILE_1;
 source $WORK_PATH/$FILE_2;
+
+source $WORK_PATH/envelope/$FILE_1;
+
+source $WORK_PATH/conversion/$FILE_0;
+source $WORK_PATH/conversion/$FILE_1;
+
 EOF
 #
 sh /docker-entrypoint-initdb.d/init_trade_history.sh1
+sh /docker-entrypoint-initdb.d/envelope/init_trade_history.sh1
+sh /docker-entrypoint-initdb.d/conversion/init_trade_history.sh1
